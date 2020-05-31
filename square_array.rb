a6a4415803b77dd9ef2df6033438da57ece39697
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 16:39:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
+
 def square_array(array)
-  array = []
-  square_array.each do |this|
-    puts new_array = this ** 2 
-  end
-  return new_array
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
